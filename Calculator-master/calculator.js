9af5f66f2e9d0c345cfdb6a0bf4349f77e4f58e1
NyarehD <nyarehdavid56@gmail.com>
2020-12-28 15:41:40 +0630

    Finished cssing calculator

--- Calculator-master/calculator.js
+++ Calculator-master/calculator.js
@@ -5,8 +5,11 @@
       // Making the all clear function
       function cls() {
         document.getElementById('inputNo').value = " ";
-      }
+      };
 // Making the calculate function works
 function calculating(){
   document.getElementById('inputNo').value=eval(document.getElementById('inputNo').value);
+};
+function clsLast() {
+ alert("This key doesn't work but I am working on this.");
 }
