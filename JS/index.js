320e83f111b02bb8abf2664cf91fe34ecf9cf82a
NyarehD <nyarehdavid56@gmail.com>
2021-05-26 16:56:53 +0630

    finished nav but might need some more

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,20 @@
-function showMenu(){
+function showMenu() {
     console.log("showed menu");
     document.getElementById("open-menu").style.display = "none";
     document.getElementById("close-menu").style.display = "initial";
     document.getElementById("sideNav").style.width = "100%";
 }
-function closeMenu(){
+function closeMenu() {
     document.getElementById("open-menu").style.display = "initial";
     document.getElementById("close-menu").style.display = "none";
     document.getElementById("sideNav").style.width = "0";
 }
-function checkLink(){
+function checkLink() {
     var link = document.querySelectorAll('a');
     var href = {};
-    for(var i = 0; i<link.length; i++){
+    for (var i = 0; i < link.length; i++) {
         href[i].push(link[i]);
     }
-    
+
     console.log(href);
 }
