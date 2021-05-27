b3ab432b5ef5df4f9505f1b048e820649ef82c9b
NyarehD <nyarehdavid56@gmail.com>
2021-05-27 19:44:48 +0630

    Remake the greeting

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,11 @@
-
-
-// $(document).ready(function () {
-//     $("#secondDiv").toggle();
-//     let navbarSecond = $('#secondDiv').waypoint(function (direction) {
-//         $('#secondDiv').toggle();
-//         console.log(direction);
-//     }, {
-//         offset: "-95%"
-//     })
-// })
-
 $(document).ready(function () {
     let secondDiv = $('#secondDiv');
     secondDiv.toggle();
     secondDiv.waypoint(function (direction) {
         secondDiv.toggle();
     }, {
-        offset: "-95%"
+        offset: "-100%"
     })
+
 })
+
