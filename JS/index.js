f0e60b4046563f46a9a8d2405facd28f5949512f
NyarehD <nyarehdavid56@gmail.com>
2021-05-28 17:24:09 +0630

    Revert "One card completed but more to come"
    This reverts commit d216f9df4fd24775b68eef9d592396b44c591497.

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     secondDiv.toggle();
     secondDiv.waypoint(function (direction) {
         secondDiv.toggle();
-        secondDiv.children("div").addClass("animate__animated animate__fadeInUp");
+        secondDiv.children("div").addClass("animate__animated animate__bounceIn");
     }, {
         offset: "-98%"
     })
