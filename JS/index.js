aa49bcf89439003e41551046ae1f991aa2712eb4
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2021-05-31 09:07:04 +0630

    Trying scrollspy

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
     let secondDiv = $('#secondDiv');
-    secondDiv.toggle();
+    secondDiv.toggleClass("invisible");
     secondDiv.waypoint(function (direction) {
-        secondDiv.toggle();
-        secondDiv.children("div").addClass("animate__animated animate__bounceIn");
+        secondDiv.toggleClass("invisible visible");
+        secondDiv.children("div").toggleClass("animate__animated animate__fadeInUp");
     }, {
-        offset: "-98%"
+        offset: "10"
     })
+    // $(".project-card").on({
+    //     mouseenter: function () {
+    //         $('.project-card').css("background-color", "rgba(95, 95, 95, 0.342)");
+    //         $(".project-text").removeClass("invisible");
+    //     },
+    //     mouseleave: function () {
+    //         $('.project-text').addClass("invisible");
+    //     }
+        
+    // });
 
 })
 
