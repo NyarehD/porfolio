87651f89322fb1247852a21755bfe502b4e9fb9e
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2021-06-05 13:21:39 +0630

    Test

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
     let secondDiv = $('#secondDiv');
     secondDiv.toggleClass("invisible");
-    secondDiv.waypoint(function (direction) {
+    secondDiv.waypoint(function () {
         secondDiv.toggleClass("invisible visible");
         secondDiv.children("div").toggleClass("animate__animated animate__fadeInUp");
-        $(`#navbarSecondContents a[href="#projects"`).addClass("active")
+        $(`#navbarSecondContents a[href="#projects"]`).addClass("active")
     }, {
         offset: "0"
     })
-    $('#about').waypoint(function (direction) {
-        $(`#navbarSecondContents a[href="#projects"]`).toggleClass('active')
-        $(`#navbarSecondContents a[href="#about"]`).toggleClass('active')
-        console.log($("#about").offset());
-
-    },{
-        offset: '10'
-    })
+    $('#about').waypoint(function () {
+            $(`#navbarSecondContents a[href="#projects"]`).toggleClass('active')
+            $(`#navbarSecondContents a[href="#about"]`).toggleClass('active')
+            console.log($("#about").offset());
+        }, {
+            offset: '10'
+        }
+    )
+    msWriteProfilerMark(secondDiv)
 
     // $(".project-card").on({
     //     mouseenter: function () {
@@ -25,7 +26,7 @@ $(document).ready(function () {
     //     mouseleave: function () {
     //         $('.project-text').addClass("invisible");
     //     }
-        
+
     // });
 
 })
