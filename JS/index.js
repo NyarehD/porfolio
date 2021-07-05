9da0159b4ec988f868b437b77aa952767a39ac45
Nyareh David <nyarehdavid56@gmail.com>
2021-07-05 21:41:06 +0630

    Added navbar link active while in sections

--- JS/index.js
+++ JS/index.js
@@ -20,15 +20,39 @@ $(document).ready(function () {
         breakpoint: "768",
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          dots: true,
         }
       }
     ]
   });
   $("#projects").waypoint(function (direction) {
-    $("#header").toggleClass("toFixed animate__animated animate__fadeInDown")
+    $("#header").toggleClass("toFixed animate__animated animate__fadeInDown shadow-3")
     console.log(direction)
+  }, {
+    offset: "10%"
   })
-
+  function setActiveWhileScrolling() {
+    let sections = $("section[id]");// For selecting section elements with id attributes
+    sections.waypoint(function (direction) {
+      let currentSection = $(this.element).attr("id");
+      (direction === "down") && setActiveInNavBar(currentSection)
+    }, {
+      offset: "10%"
+    });
+    sections.waypoint(function (direction) {
+      let currentSection = $(this.element).attr("id");  
+      (direction === "up") && setActiveInNavBar(currentSection)
+    }, {
+      offset: "-100%"
+    })
+    function setActiveInNavBar(currentSectionId) {
+      $(".nav-link").removeClass("f-orangy");
+      $(`.nav-link[href='#${currentSectionId}']`).addClass("f-orangy")
+      console.log(currentSectionId);
+    }
+    console.log("What the fuck");
+  }
+  setActiveWhileScrolling();
 })
 
