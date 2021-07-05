ec9410b26562b62c85b45e30d305b47db8b507eb
Nyareh David <nyarehdavid56@gmail.com>
2021-07-05 15:37:22 +0630

    added slick and made nav fixed

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
 
+  $('.project-cards').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }, {
+        breakpoint: "768",
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $("#projects").waypoint(function (direction) {
+    $("#header").toggleClass("toFixed animate__animated animate__fadeInDown")
+    console.log(direction)
+  })
+
 })
 
