95d757c1a8fc988c9c6e15070c8d11a6ecebe6d1
Nyareh David <nyarehdavid56@gmail.com>
2021-07-07 16:42:36 +0630

    Added transition when hover over nav links

--- JS/index.js
+++ JS/index.js
@@ -1,66 +1,37 @@
 $(document).ready(function () {
-  $('.project-cards').slick({
-    arrows: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true
-        }
-      }, {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: true,
-        }
-      }
-    ]
-  });
+  // For animation to be activated when user reaches the content
+  new WOW().init();
 
-
-  // $("#projects").waypoint(function (direction) {
-  //   $("#header").toggleClass("toFixed b-shadow")
-  // }, {
-  //   offset: "10%"
-  // })
-
-  // Global variable
+  /**
+   * Global variable for scrolling up or down
+   */
   let previousSection;
+
+  // For Navbar to be revealed when scrolling up and hide when scrolling down
   $(window).scroll(function (event) {
     let current = $(this).scrollTop();
 
-    // To hide navbar while in header section
+    // To hide navbar while in header section whether scrolling up or down
     if ((this.innerHeight * 0.9) >= current) {
       $('#header').addClass("toFixedHide").removeClass("toFixedShow b-shadow")
-      console.log(1);
     } else {
       // To hide navbar while scrolling down
       if (previousSection < current) {
         $('#header').addClass("toFixedHide").removeClass("toFixedShow b-shadow")
-        console.log(2);
       } else if (previousSection >= current) {
         /**
          * The reason why I didn't use else is that the classes can be messed up like below
-        *So by default, make the 2 and 3 to trigger simutaneously
-        *Because we haven't assigned previousSection yet
+         * So by default, make the 2 and 3 to trigger simutaneously
+         * Because we haven't assigned previousSection yet
          */
         // To show navbar while scrolling up
         $('#header').addClass("toFixedShow b-shadow").removeClass("toFixedHide")
-        console.log(3);
       }
     }
     previousSection = $(this).scrollTop();
   })
+
+  // For changing active link color in Navbar
   function setActiveWhileScrolling() {
     let sections = $("section[id]");// For selecting section elements with id attributes
     sections.waypoint(function (direction) {
@@ -81,7 +52,35 @@ $(document).ready(function () {
     }
   }
   setActiveWhileScrolling();
-  new WOW().init();
+
+  // For slick js which is used for project cards carousel
+  $('.project-cards').slick({
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      }
+    ]
+  });
 })
 
 
