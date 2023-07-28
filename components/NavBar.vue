947b7aa2a9ef05adc004ea94aca096289ea532f2
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-07-28 17:29:42 +0630

    Fix: NavBar Not working

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="bg-priBg sticky top-0 z-20" ref="navbar">
-    <div class="md:px-12 lg:px-16 flex justify-between px-8 py-3">
+  <nav class="fixed top-0 w-full" ref="navbar">
+    <div class="md:px-12 lg:px-16 bg-priBg relative z-40 flex justify-between px-8 py-3">
       <div class="">
         <img src="~/assets/images/logo.png" alt="" class="h-12">
       </div>
@@ -19,17 +19,17 @@
         </button>
       </div>
     </div>
+    <!-- This is collapsed navbar options in small device -->
+    <Transition name="slide">
+      <div class="bg-priBg md:hidden -z-10 px-8 py-3" v-if="isNavShown">
+        <ul class="lg:space-x-5 flex flex-col w-full mx-auto space-y-2 align-baseline">
+          <NavLink :route-name="route.name">Home</NavLink>
+          <NavLink :route-name="route.name" to="projects">Projects</NavLink>
+          <NavLink :route-name="route.name" to="about">About</NavLink>
+        </ul>
+      </div>
+    </Transition>
   </nav>
-  <!-- This is collapsed navbar options in small device -->
-  <Transition name="slide">
-    <div class="bg-priBg md:hidden z-10 px-8 py-3" v-if="isNavShown">
-      <ul class="lg:space-x-5 flex flex-col w-full mx-auto space-y-2 align-baseline">
-        <NavLink :route-name="route.name">Home</NavLink>
-        <NavLink :route-name="route.name" to="projects">Projects</NavLink>
-        <NavLink :route-name="route.name" to="about">About</NavLink>
-      </ul>
-    </div>
-  </Transition>
 </template>
 <script setup lang="ts">
 
