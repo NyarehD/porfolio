0639d0b80e38abefb473ce2a8430a3f49702458e
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-07-28 18:31:24 +0630

    Add: NavBar show & hide

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
-  <nav class="fixed top-0 w-full" ref="navbar">
-    <div class="md:px-12 lg:px-16 bg-priBg relative z-40 flex justify-between px-8 py-3">
+  <nav class=" top-0 w-full" ref="navbar" :class="{ 'navBarHide': !windowScrollY, 'navBarAppear': windowScrollY }">
+    <div class="md:px-12 lg:px-16 relative z-40 flex justify-between px-8 py-3"
+      :class="{ 'bg-transparent': !windowScrollY, 'bg-priBg': windowScrollY || isNavShown }">
       <div class="">
         <img src="~/assets/images/logo.png" alt="" class="h-12">
       </div>
@@ -20,8 +21,9 @@
       </div>
     </div>
     <!-- This is collapsed navbar options in small device -->
-    <Transition name="slide">
-      <div class="bg-priBg md:hidden -z-10 px-8 py-3" v-if="isNavShown">
+    <Transition :name="windowScrollY ? 'slide' : 'fade'">
+      <div class=" md:hidden -z-10 px-8 py-3"
+        :class="{ 'bg-transparent': !windowScrollY, 'bg-priBg': windowScrollY || isNavShown }" v-if="isNavShown">
         <ul class="lg:space-x-5 flex flex-col w-full mx-auto space-y-2 align-baseline">
           <NavLink :route-name="route.name">Home</NavLink>
           <NavLink :route-name="route.name" to="projects">Projects</NavLink>
@@ -35,6 +37,13 @@
 
   let isNavShown = ref(false);
   let route = useRoute();
+
+  let windowScroll = useWindowScroll();
+  let windowScrollY = computed(() => {
+    return windowScroll.y.value > 500;
+  })
+
+  let windowSize = useWindowSize();
 </script>
 <style lang="scss">
   .slide-enter-active,
@@ -47,5 +56,61 @@
   .slide-leave-to {
     transform: translateY(-100%);
     transition: 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97);
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    opacity: 1;
+    transition: 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97);
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    transition: 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97);
+  }
+
+  .navBarAppear {
+    transition: all 1s;
+    position: fixed;
+    animation-name: navBarAppear;
+    animation-duration: 1s;
+    animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  @keyframes navBarAppear {
+    0% {
+      top: -100%;
+      opacity: 0;
+    }
+
+    100% {
+      top: 0%;
+      opacity: 1;
+    }
+  }
+
+  .navBarHide {
+    transition: all 1s;
+    position: absolute;
+    animation-name: navBarHide;
+    animation-duration: 1s;
+    animation-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  @keyframes navBarHide {
+    0% {
+      top: 0%;
+      opacity: 1;
+    }
+
+    50% {
+      top: -100%;
+      opacity: 0;
+    }
+
+    100% {
+      top: 0;
+    }
   }
 </style>
