ca6bf92541762253ae72b5f65ade6adf5af03e86
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-08-07 22:06:42 +0630

    Update: hide navigation for only one photos

--- components/ProjectImages.vue
+++ components/ProjectImages.vue
@@ -1,21 +1,23 @@
 <template>
   <div class=" h-1/2 lg:h-auto bg-secBg lg:w-1/2 relative w-full">
-    <ArrowButton
-      class="top-1/2 bg-primary text-whity left-5 hover:scale-105 absolute transition duration-100 rotate-90 -translate-y-full"
-      @click="() => currentPhotoId > 0 && currentPhotoId--" />
     <Transition name="carousel">
       <NuxtImg :src="`/projects/${photos[currentPhotoId]}`" :alt="photos[currentPhotoId]" sizes="xs:700px xl:800px"
         fit="cover" class=" md:object-contain object-cover h-full mx-auto" />
     </Transition>
-    <ArrowButton
-      class="top-1/2 bg-primary text-whity right-5 hover:scale-105 absolute transition duration-100 -rotate-90 -translate-y-full cursor-pointer"
-      @click="() => currentPhotoId < props.photos.length - 1 && currentPhotoId++" />
-    <div class="left-1/2 absolute bottom-0 flex -translate-x-1/2">
-      <div v-for="(photo, i) in photos.length" :key="photo" class=" w-3 h-3 m-3 cursor-pointer"
-        :class="{ 'bg-primary': i === currentPhotoId, 'bg-whity': i !== currentPhotoId }"
-        @click="() => currentPhotoId = i">
+    <template v-if="props.photos.length > 1">
+      <ArrowButton
+        class="top-1/2 bg-primary text-whity left-5 hover:scale-105 absolute transition duration-100 rotate-90 -translate-y-full"
+        @click="() => currentPhotoId > 0 && currentPhotoId--" />
+      <ArrowButton
+        class="top-1/2 bg-primary text-whity right-5 hover:scale-105 absolute transition duration-100 -rotate-90 -translate-y-full cursor-pointer"
+        @click="() => currentPhotoId < props.photos.length - 1 && currentPhotoId++" />
+      <div class="left-1/2 absolute bottom-0 flex -translate-x-1/2">
+        <div v-for="(photo, i) in photos.length" :key="photo" class=" w-3 h-3 m-3 cursor-pointer"
+          :class="{ 'bg-primary': i === currentPhotoId, 'bg-whity': i !== currentPhotoId }"
+          @click="() => currentPhotoId = i">
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
