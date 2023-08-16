a20829f600ce97d894f87e41c0d3689ea2f93d50
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2023-08-16 19:21:16 +0630

    Add: Project link in index

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,19 @@
       <span class="text-whity mix-blend-difference md:mix-blend-normal md:text-5xl text-4xl font-normal">OH, Hi
         There!</span>
       <div class="">
-        <span class="md:text-9xl text-whity text-7xl">I'm </span>
+        <span class="md:text-9xl text-whity text-7xl md:mix-blend-normal mix-blend-difference">I'm </span>
         <span class="text-primary md:text-9xl mix-blend-difference text-7xl">Nyareh</span>
       </div>
       <div class="text-ellipsis md:w-3/4">
-        <span class=" text-whity md:text-7xl text-4xl font-normal">Who's learning to be </span>
-        <span class="text-primary md:text-7xl text-4xl font-normal">a web developer</span>
+        <span class=" text-whity md:text-6xl md:mix-blend-normal mix-blend-difference text-4xl font-normal">Who's
+          enthusiastic about </span>
+        <span class="text-primary md:text-6xl mix-blend-difference md:mix-blend-normal text-4xl font-normal">web
+          development</span>
       </div>
-    </div>
-    <ArrowButton class="text-secondary animate-bounce bottom-4 bg-secBg absolute left-0 right-0 w-10 h-10" />
+      <span class="md:text-4xl text-whity text-3xl">Learn more about my <NuxtLink :to="{ name: 'projects' }"
+          class="project-link text-ternary before:bg-whity md:px-3 p-2">
+          Projects
+        </NuxtLink></span>
     </div>
   </div>
 </template>
@@ -23,5 +27,36 @@
 <style lang="scss">
   .backgroundImg {
     background-image: url("~/assets/images/background.svg");
+    background-attachment: fixed;
+  }
+
+  @mixin mixBlendModeEffect($transformScaleY, $transformOrigin) {
+    position: relative;
+    isolation: isolate;
+
+    &::before {
+      content: "";
+      position: absolute;
+      inset: 0;
+      mix-blend-mode: difference;
+      transition: transform 0.25s;
+      transform: scaleY($transformScaleY);
+      transform-origin: $transformOrigin;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &:focus,
+    &:hover {
+      &::before {
+        transform: none;
+      }
+    }
+  }
+
+  .project-link {
+    @include mixBlendModeEffect(0, 0 50%)
   }
 </style>
